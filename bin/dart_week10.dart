a98f7d745559fab5dd67bd4077178d6a9d17893a
coach24546 <wikod71@gmail.com>
2023-07-20 16:09:05 +0700

    first commit

---
+++ bin/dart_week10.dart
@@ -0,0 +1,9 @@
+import 'package:dart_week10/Generatio.dart';
+import 'package:dart_week10/generaions.dart';
+
+void main() {
+  bmiDemo();
+  generationsDemo();
+  // listIfElseIfDemo();
+  // ifElseIfDemo();
+  // ifElseDemo();
---
+++ lib/dart_week10.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+void generationsDemo() {
+  int birth;
+  String message = "";
+  List<String> myGenerations = [
+    "Lost Generation",
+    "Greatest Generation",
+    "ERROR",
+  ];
+  print("คุณเกิดปี พ.ศ.:");
+  birth = int.parse(stdin.readLineSync()!);
+  if (birth >= 2426 && birth <= 2443) {
+    message = "คุณเกิดปี พ.ศ. $birth:คุณเป็นคน Lost Generation:${myGenerations[0]}";
+  } else if (birth >= 2426 && birth <= 2443) {
+    message = "คุณเกิดปี พ.ศ. $birth:คุณเป็นคน Greatest Generation:${myGenerations[1]}";
+  } else if (birth >= 2444 && birth <= 2467) {
+    message = "คุณเกิดปี พ.ศ. $birth:คุณเป็นคน Silent Generation:${myGenerations[2]}";
+  } else if (birth >= 2468 && birth <= 2488) {
+    message = "คุณเกิดปี พ.ศ. $birth:คุณเป็นคน Baby Boomer:${myGenerations[3]}";
+  } else if (birth >= 2489 && birth <= 2507) {
+    message = "คุณเกิดปี พ.ศ. $birth:คุณเป็นคน Generation X:${myGenerations[4]}";
+  } else if (birth >= 2508 && birth <= 2522) {
+    message = "คุณเกิดปี พ.ศ. $birth:คุณเป็นคน Generation Y:${myGenerations[5]}";
+  } else if (birth >= 2523 && birth <= 2540) {
+    message = "คุณเกิดปี พ.ศ. $birth:คุณเป็นคน Generation Z:${myGenerations[6]}";
+  } else if (birth >= 2523 && birth <= 2540) {
+    message = "คุณเกิดปี พ.ศ. $birth:คุณเป็นคน Generation C:${myGenerations[7]}";
+  }else if (birth >= 2523 && birth <= 2540) {
+
+  print(message);
+}
